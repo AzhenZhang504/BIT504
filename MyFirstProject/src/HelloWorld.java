a297fddb68167ce27ef6fc6a445500d7689acfa3
AzhenZhang504 <369159004@qq.com>
2022-12-29 15:32:26 +1300

    The second change

--- MyFirstProject/src/HelloWorld.java
+++ MyFirstProject/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// Change the comment on line 5
+		// Change the comment on line 5 
 		System.out.println("Hello World");
 
 	}
